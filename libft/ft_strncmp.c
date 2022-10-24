00c1fdc01af71c0785348c87267732ad6144a54a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-10-24 22:35:05 +0200

    updated strncmp to handle non-ascii (cast to unsigned char)

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/23 19:46:12 by fras          #+#    #+#                 */
-/*   Updated: 2022/10/24 13:44:46 by fras          ########   odam.nl         */
+/*   Updated: 2022/10/24 22:32:52 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	chr1;
+	unsigned char	chr2;
 
 	i = 0;
 	if (s1 == s2 || n == 0)
 		return (0);
 	while (s1[i] == s2[i] && i < n - 1 && s1[i] && s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	chr1 = (unsigned char)s1[i];
+	chr2 = (unsigned char)s2[i];
+	return (chr1 - chr2);
 }
-
-	//	Other variant:
-	//
-	//	if (s1[i] > s2[i])
-	//  	return (1);
-	//	if (s1[i] < s2[i])
-	//  	return (-1);
-	//	else
-	// 		return (0);
