ba198439d9cffdd83e6799211e938a324ca603b4
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-10-28 23:20:08 +0200

    updates (wip)

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,25 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/27 12:52:49 by fras          #+#    #+#                 */
-/*   Updated: 2022/10/28 18:16:04 by fras          ########   odam.nl         */
+/*   Updated: 2022/10/28 23:19:44 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	countc(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s)
+	{
+		s++;
+		if (*s == c)
+			i++;
+	}
+	return (i);
+}
 
 static size_t	strlenskipc(char const *s, char c)
 {
@@ -30,19 +44,29 @@ char	**ft_split(char const *s, char c)
 {
 	char	**ret;
 	size_t	i;
-	size_t	j;
-	size_t	k;
+	size_t	start;
+	size_t	arrlen;
+	size_t	srclen;
 
 	i = 0;
-	j = 0;
-	k = 0;
-	ret = malloc(strlenskipc(s, c) + 1);
-	while (s[i])
+	start = 0;
+	arrlen = (countc(s, c) + 1);
+	srclen = (strlenskipc(s, c) + arrlen);
+	ret = malloc(countc(s, c) + 1);
+	if (!**ret)
+		return (NULL);
+	else
+		ret[arrlen] = NULL;
+	while (**ret)
 	{
-		if (s[i] != c)
-			ret[j][k++] = s[i++];
-		else
-			j++;
+		*ret = ft_substr(s, start, );
+		while (*s != c)
+			start++;
+		ret++;
 	}
 	return (ret);
 }
+
+// Hallo Hallo Hallo Hallo
+
+// 0 > 1 > 2 > 3 >  4
