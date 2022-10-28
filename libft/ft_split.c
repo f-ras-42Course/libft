e9f0babd288464b86c0f28fde77842bc4c48fc7c
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-10-28 15:49:37 +0200

    wip

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,42 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/27 12:52:49 by fras          #+#    #+#                 */
-/*   Updated: 2022/10/27 15:31:44 by fras          ########   odam.nl         */
+/*   Updated: 2022/10/28 15:47:26 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static size_t	strlenskipc(char const *s, char c)
+{
+	size_t	i;
+
+	while (*s)
+	{
+		*s++;
+		if (*s != c)
+			i++;
+	}
+	return (i);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**ret;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	ret = malloc(strlenskipc(s, c) + 1);
+	while (s[i])
+	{
+		if (s[i] != c)
+			ret[j][k++] = s[i++];
+		else
+			j++;
+	}
+	return (ret);
 }
