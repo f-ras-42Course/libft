bf2c7b38b4a9d0f0e180e9aeacf750111fee1b88
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-11-07 01:04:58 +0100

    norm fix

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,16 +6,15 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/07 00:30:47 by fras          #+#    #+#                 */
-/*   Updated: 2022/11/07 00:56:35 by fras          ########   odam.nl         */
+/*   Updated: 2022/11/07 01:04:14 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_lstsize(t_list *lst)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (lst)
@@ -23,5 +22,5 @@ int	ft_lstsize(t_list *lst)
 		lst = lst->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }
