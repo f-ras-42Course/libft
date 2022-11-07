82ebb94261fa6df8a757af748bbf5794c8e85f08
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-11-07 21:08:37 +0100

    added lstiter

---
+++ libft/ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstiter.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fras <fras@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/07 20:55:39 by fras          #+#    #+#                 */
+/*   Updated: 2022/11/07 21:08:15 by fras          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst)
+		return ;
+	(*f)(lst->content);
+	while (lst->next)
+	{
+		lst = lst->next;
+		(*f)(lst->content);
+		if (!lst->next)
+			break ;
+	}
+}
