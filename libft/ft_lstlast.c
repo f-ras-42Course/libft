c20f9e72a99e2eda3312edb53ca2b63889d57464
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2022-11-07 21:36:00 +0100

    simplified func

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/07 01:03:06 by fras          #+#    #+#                 */
-/*   Updated: 2022/11/07 02:11:33 by fras          ########   odam.nl         */
+/*   Updated: 2022/11/07 21:35:11 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ t_list	*ft_lstlast(t_list *lst)
 	if (!lst)
 		return (NULL);
 	while (lst->next)
-	{
 		lst = lst->next;
-		if (!lst->next)
-			break ;
-	}
 	return (lst);
 }
