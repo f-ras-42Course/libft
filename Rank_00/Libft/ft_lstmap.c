934f1f71ff730997d951dfd91fa4baf5092df2b9
Ferry Ras <fras@student.codam.nl>
2022-11-12 13:30:28 +0100

    finished lstmap, need to clear up

--- Rank_00/Libft/ft_lstmap.c
+++ Rank_00/Libft/ft_lstmap.c
@@ -6,30 +6,39 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/07 21:09:10 by fras          #+#    #+#                 */
-/*   Updated: 2022/11/12 13:03:36 by fras          ########   odam.nl         */
+/*   Updated: 2022/11/12 13:30:07 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap (t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new;
-	t_list *firstnode;
+	t_list	*new;
+	t_list	*firstnode;
+	void	*ptr;
 
 	if (!lst)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content));
-	if (!new)
+	ptr = (*f)(lst->content);
+	if (!ptr)
 		return (NULL);
+	new = ft_lstnew(ptr);
+	if (!new)
+	{
+		(*del)(ptr);
+		return (NULL);
+	}
 	firstnode = new;
 	while (lst->next)
 	{
 		lst = lst->next;
-		new->next = ft_lstnew((*f)(lst->content));
+		ptr = (*f)(lst->content);
+		new->next = ft_lstnew(ptr);
 		new = new->next;
 		if (!new)
 		{
+			(*del)(ptr);
 			ft_lstclear(&firstnode, del);
 			return (NULL);
 		}
