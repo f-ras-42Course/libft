09447a5e20c8e6db03eb51ca9bd7c74eeef4ca96
Ferry Ras <fras@student.codam.nl>
2022-11-13 20:50:17 +0100

    finished lstmap

--- Rank_00/libft/ft_lstmap.c
+++ Rank_00/libft/ft_lstmap.c
@@ -6,42 +6,45 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/07 21:09:10 by fras          #+#    #+#                 */
-/*   Updated: 2022/11/12 13:30:07 by fras          ########   odam.nl         */
+/*   Updated: 2022/11/13 20:50:04 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static t_list	*mapnewnode(t_list *lst, void *(*f)(void *))
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = (*f)(lst->content);
+	new->next = NULL;
+	return (new);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*firstnode;
-	void	*ptr;
+	t_list	*firstnode_new;
 
 	if (!lst)
 		return (NULL);
-	ptr = (*f)(lst->content);
-	if (!ptr)
-		return (NULL);
-	new = ft_lstnew(ptr);
+	new = mapnewnode(lst, f);
 	if (!new)
-	{
-		(*del)(ptr);
 		return (NULL);
-	}
-	firstnode = new;
+	firstnode_new = new;
 	while (lst->next)
 	{
 		lst = lst->next;
-		ptr = (*f)(lst->content);
-		new->next = ft_lstnew(ptr);
+		new->next = mapnewnode(lst, f);
 		new = new->next;
 		if (!new)
 		{
-			(*del)(ptr);
-			ft_lstclear(&firstnode, del);
+			ft_lstclear(&firstnode_new, del);
 			return (NULL);
 		}
 	}
-	return (firstnode);
+	return (firstnode_new);
 }
