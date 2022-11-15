5b26e7c6f1ea389b29a7874433c2394bb2602fef
Ferry Ras <fras@student.codam.nl>
2022-11-15 17:42:34 +0100

    cleanup putnbr

--- Rank_00/libft/ft_putnbr_fd.c
+++ Rank_00/libft/ft_putnbr_fd.c
@@ -6,27 +6,22 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/05 14:16:06 by fras          #+#    #+#                 */
-/*   Updated: 2022/11/05 14:53:33 by fras          ########   odam.nl         */
+/*   Updated: 2022/11/15 17:42:18 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	putnbr(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 10 && n >= 0)
 	{
-		putnbr(n + '0', fd);
+		ft_putchar_fd(n + '0', fd);
 	}
 	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		putnbr(n % 10 + '0', fd);
+		ft_putchar_fd(n % 10 + '0', fd);
 	}
 	if (n < 0)
 	{
@@ -34,7 +29,7 @@ void	ft_putnbr_fd(int n, int fd)
 			write(fd, "-2147483648", 11);
 		else
 		{	
-			putnbr('-', fd);
+			ft_putchar_fd('-', fd);
 			n *= -1;
 			ft_putnbr_fd(n, fd);
 		}
